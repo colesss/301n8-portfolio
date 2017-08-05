8c26b70726e21291e99af1311821eb7016d0907d
Cole Allen <decolesss@gmail.com>
2017-08-05 13:55:43 -0700

    added handlebars template

--- scripts/newSourceIdea.js
+++ scripts/newSourceIdea.js
@@ -2,21 +2,24 @@
 
 var newProjects = [
   {
-    projectName: 'projectOneName'
-    dateCreated: 'xx/xx/xxxx'
+    projectName: 'projectTwoName'
+    dateCreated: 'xxxx/xx/xx'
+    projectBio: 'project description'
     newSkills: 'skill this, skill that, skill etc.'
-    fileLocation: 'https? or save project file in this backend?'
+    projectUrl: 'https? or save project file in this backend?'
   },
   {
     projectName: 'projectTwoName'
-    dateCreated: 'xx/xx/xxxx'
+    dateCreated: 'xxxx/xx/xx'
+    projectBio: 'project description'
     newSkills: 'skill this, skill that, skill etc.'
-    fileLocation: 'https? or save project file in this backend?'
+    projectUrl: 'https? or save project file in this backend?'
   },
   {
-    projectName: 'projectThreeName'
-    dateCreated: 'xx/xx/xxxx'
+    projectName: 'projectTwoName'
+    dateCreated: 'xxxx/xx/xx'
+    projectBio: 'project description'
     newSkills: 'skill this, skill that, skill etc.'
-    fileLocation: 'https? or save project file in this backend?'
+    projectUrl: 'https? or save project file in this backend?'
   }
 ]
